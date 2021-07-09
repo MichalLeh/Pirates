d8a2332c8dac5b3ef063570782446edd9b657f2f
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2021-07-09 12:29:41 +0200

    Added getCrewSize method

--- src/com/gfa/exam/Ship.java
+++ src/com/gfa/exam/Ship.java
@@ -5,5 +5,11 @@ import java.util.ArrayList;
 public class Ship {
     private ArrayList<Pirate> pirates;
 
+    public Ship() {
+        this.pirates = new ArrayList<>();
+    }
 
+    public int getCrewSize(){
+        return this.pirates.size();
+    }
 }
