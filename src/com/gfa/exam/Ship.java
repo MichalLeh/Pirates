38e0a7bf60245fe555b83449a2ad6db1ed1046da
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2021-07-09 12:40:02 +0200

    Added getPoorPirates, getGolds,LastDayOnTheShip methods

--- src/com/gfa/exam/Ship.java
+++ src/com/gfa/exam/Ship.java
@@ -4,12 +4,37 @@ import java.util.ArrayList;
 
 public class Ship {
     private ArrayList<Pirate> pirates;
+    private ArrayList<String> poorPirates;
 
     public Ship() {
         this.pirates = new ArrayList<>();
+        this.poorPirates = new ArrayList<>();
     }
 
     public int getCrewSize(){
         return this.pirates.size();
+    }
+
+    public ArrayList<String> getPoorPirates(){
+        for(Pirate pirate : this.pirates){
+            if(pirate.getHasWoodenLeg() && pirate.getGoldAmount() < 15){
+                this.poorPirates.add(pirate.getName());
+            }
+        }
+        return this.poorPirates;
+    }
+
+    public int getGolds(){
+        int sumOfGold = 0;
+        for(Pirate pirate : this.pirates){
+            sumOfGold += pirate.getGoldAmount();
+        }
+        return sumOfGold;
+    }
+
+    public void lastDayOnTheShip(){
+        for(Pirate pirate : this.pirates){
+            pirate.party();
+        }
     }
 }
